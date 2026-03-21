27ebb9d6c960c12202cf789671441f0bb6f92139
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-03-21 10:34:32 -0400

    fix: final polish of declination logic and ruby style

--- methodDeclination.rb
+++ methodDeclination.rb
@@ -1,36 +1,41 @@
 # Copyright (C) 2026 my-app-s
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# Licensed under the GNU AGPLv3
 
- total_number = ARGV[0].to_i
+# Get the number from command line arguments.
+# Defaults to 0 if no argument is provided.
+total_number = ARGV[0].to_i
 
+# Returns a string with the correctly declined noun based on the provided number.
+#
+# @param number [Integer] The number to determine the noun's form.
+# @param form_one [String] Nominative singular (e.g., "crocodile").
+# @param form_two [String] Genitive singular (e.g., "crocodiles" in some contexts).
+# @param form_three [String] Genitive plural (e.g., "crocodiles").
+#
+# @return [String] A formatted string: "Number Noun!"
+#
+# @example
+#   declination(12, "apple", "apples", "apples") #=> "12 apples!"
+#
 def declination(number, form_one, form_two, form_three)
-    # hand over in function arguments number, one, two and tree declination (total four argument)
-    if number == nil or !number.is_a?(Numeric)
-        number = 0
-    end
+  number = number.to_i
+  
+  # Handle Russian language exceptions for numbers 11 to 14
+  if (number % 100).between?(11, 14)
+    return "#{number} #{form_three}!"
+  end
 
-    remainder = number % 10
+  remainder = number % 10
 
-    if remainder == 1
-        return  "#{number} #{form_one}!"
-    elsif remainder > 1 and remainder <= 4
-        return  "#{number} #{form_two}!"
-    elsif remainder > 4 or remainder == 0
-        return  "#{number} #{form_three}!"
-    end
+  case remainder
+  when 1
+    "#{number} #{form_one}!"
+  when 2..4
+    "#{number} #{form_two}!"
+  else
+    "#{number} #{form_three}!"
+  end
 end
 
-
-puts declination(total_number, "крокодил", "крокодила", "крокодилов")
+# Output the result to the console
+puts declination(total_number, "crocodile", "crocodiles", "crocodiles")
